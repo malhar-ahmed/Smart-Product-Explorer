cccf6470cc545b85a28085b87f22bff14eb26d7a
Malhar Ahmed <malharahmed2003@gmail.com>
2026-03-06 22:59:32 +0530

    Created product card component and seperated ui

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import type { Product } from './types/Product'
+import ProductCard from './components/ProductCard.vue'
 
 const products = ref<Product[]>([])
-const loading = ref(true)
 
 onMounted(async () => {
   const res = await fetch('https://dummyjson.com/products')
   const data = await res.json()
   products.value = data.products
-  loading.value = false
 })
 </script>
 
@@ -17,18 +16,12 @@ onMounted(async () => {
   <div class="p-6">
     <h1 class="text-2xl font-bold mb-4">Smart Product Explorer</h1>
 
-    <div v-if="loading">Loading...</div>
-
-    <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-4">
-      <div
-        v-for="product in products"
-        :key="product.id"
-        class="border p-4 rounded shadow"
-      >
-        <img :src="product.thumbnail" class="h-40 w-full object-cover mb-2" />
-        <h2 class="font-bold">{{ product.title }}</h2>
-        <p>${{ product.price }}</p>
-      </div>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <ProductCard
+        v-for="p in products"
+        :key="p.id"
+        :product="p"
+      />
     </div>
   </div>
 </template>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { Product } from '../types/Product'
+
+defineProps<{
+  product: Product
+}>()
+</script>
+
+<template>
+  <div class="border p-4 rounded shadow hover:shadow-lg transition">
+    <img :src="product.thumbnail" class="h-40 w-full object-cover mb-2" />
+    <h2 class="font-bold">{{ product.title }}</h2>
+    <p class="text-gray-500">${{ product.price }}</p>
+  </div>
+</template>

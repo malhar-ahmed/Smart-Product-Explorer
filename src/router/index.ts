9a8da4af8fe8e8a73a987299c93063bac1e6eabe
Malhar Ahmed <malharahmed2003@gmail.com>
2026-03-08 01:01:27 +0530

    setup vue router and started product detail page

---
+++ src/router/index.ts
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../App.vue'
+import ProductDetail from '../pages/ProductDetail.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: Home },
+    { path: '/product/:id', component: ProductDetail }
+  ]
+})
+
+export default router
